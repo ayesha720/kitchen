5f226d716dbf19593907af8c4856687f2ee282ec
ayesha720 <51579690+ayesha720@users.noreply.github.com>
2024-11-14 11:53:02 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import {Routes,Route} from 'react-router-dom'
-import Home from './Pages/Home/Home'
-import Cart from './Pages/Cart/Cart'
 
-import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
-import Footer from './Components/Footer/Footer'
+
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home/Home';
+import Cart from './Pages/Cart/Cart';
+import PlaceOrder from './Pages/PlaceOrder/PlaceOrder';
+import Footer from './Components/Footer/Footer';
 import LoginSignUp from './Pages/LoginSignUp';
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <div className='app'>
-     <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/cart' element={<Cart />}/>
-      <Route path='/order' element={<PlaceOrder />}/>
-      <Route path='/login' element={<LoginSignUp />}/>
-      
-    </Routes>
-    </div>
-    
-    <Footer/>
-    </>
-  
-  )
-}
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+          <Route path='/login' element={<LoginSignUp />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
+
